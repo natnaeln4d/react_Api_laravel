f45263a364550fb0a80f0ffc1215926b50a0fd3c
Natnael Getachew <natnaeln4dgmail.com>
2023-01-03 22:15:27 +0300

    adding src/componets/Users.js file

---
+++ src/componets/Users.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+
+export default class Users extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        items: []
+      };
+    }
+  
+    componentDidMount() {
+      fetch("http://127.0.0.1:8000/api/posts")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              items: result.items
+            });
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+    }
+  
+    render() {
+      const { error, isLoaded, items } = this.state;
+      if (error) {
+        return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+        return (
+          <ul>
+            {items.map(item => (
+              <li key={item.id}>
+                {item.name} {item.price}
+              </li>
+            ))}
+          </ul>
+        );
+      }
+    }
+  }
