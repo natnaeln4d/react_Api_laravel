b71778efbc86b4ed851b93abefd7785ac42fd007
Natnael Getachew <natnaeln4d@gmail.com>
2023-01-19 18:51:01 +0300

    modifing src/componets/authHeader.js

--- src/componets/authHeader.js
+++ src/componets/authHeader.js
@@ -1,6 +1,6 @@
 export function authHeader() {
     // return authorization header with basic auth credentials
-    let user = JSON.parse(localStorage.getItem('token'));
+    let user = JSON.parse(localStorage.getItem('loged_in_status'));
 
     if (user && user.token) {
         return { Authorization: `Bearer ${user.token}` };
